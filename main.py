b43b65e6415017e9a9bdae6a4c982d456e5e53ca
Phillip Le Ross <phillip.leross@gmail.com>
2025-06-23 20:38:42 -0700

    Add main.py Flask server for Render

--- main.py
+++ main.py
@@ -1,17 +1,15 @@
-# main.py
 from flask import Flask, send_from_directory
 import os
 
-app = Flask(__name__, static_folder='public', template_folder='public')
+app = Flask(__name__, static_folder='public')
 
 @app.route('/')
-def index():
-    return send_from_directory('public', 'index.html')
+def serve_index():
+    return send_from_directory(app.static_folder, 'index.html')
 
-@app.route('/<path:path>')
-def serve_file(path):
-    return send_from_directory('public', path)
+@app.route('/<path:filename>')
+def serve_file(filename):
+    return send_from_directory(app.static_folder, filename)
 
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port)
+    app.run(debug=False, host='0.0.0.0', port=10000)
